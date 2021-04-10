c6eb04189376ee7c54245a2215a496a1cd3f387b
fly <fly@load-page.com>
2021-04-10 21:57:19 +0800

    move controller.CallMethod to callControllerMethod

--- web/controllers/controller.go
+++ web/controllers/controller.go
@@ -1,10 +1,7 @@
 package controllers
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
-	"go-common/utils"
-	"reflect"
 	"time"
 )
 
@@ -15,7 +12,6 @@ type Controller struct {
 type ControllerInterface interface {
 	JsonSuccessResponse(data interface{})
 	JsonErrorResponse(code int, message string, data interface{})
-	CallMethod(controller ControllerInterface, method string, args ...interface{}) (interface{}, *ResponseException)
 }
 
 func (c *Controller) JsonErrorResponse(code int, message string, data interface{}) {
@@ -40,26 +36,6 @@ func (c *Controller) JsonSuccessResponse(data interface{}) {
 		Duration: float64(duration) / float64(time.Second),
 		At:       time.Now().UnixNano() / int64(time.Millisecond),
 	})
-}
-
-func (c *Controller) CallMethod(controller ControllerInterface, method string, args ...interface{}) (interface{}, *ResponseException) {
-	res, err := utils.CallMethod2(controller, method, args...)
-
-	errKind := reflect.ValueOf(err).Kind()
-	if errKind == reflect.Ptr && !reflect.ValueOf(err).IsNil() {
-		switch reflect.TypeOf(err).Elem().Name() {
-		case "errorString":
-			return res, NewResponseException(-1, err.(error).Error())
-		case "ResponseException":
-			return res, err.(*ResponseException)
-		default:
-			return res, NewResponseException(-1, fmt.Sprintf("%#v", err))
-		}
-	} else if errKind == reflect.String && err != "" {
-		return res, NewResponseException(-1, err.(string))
-	}
-
-	return res, nil
 }
 
 func (c *Controller) shouldBindQuery() {
--- web/controllers/handle.go
+++ web/controllers/handle.go
@@ -3,6 +3,8 @@ package controllers
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"go-common/utils"
+	"reflect"
 )
 
 var controllerRegistry map[string]func(ctx *gin.Context) ControllerInterface
@@ -27,10 +29,30 @@ func ControllerHandle(controllerName, methodName string) func(ctx *gin.Context) 
 	return func(ctx *gin.Context) {
 		controller := NewController(controllerName, ctx)
 
-		if res, err := controller.CallMethod(controller, methodName); err == nil {
+		if res, err := callControllerMethod(controller, methodName); err == nil {
 			controller.JsonSuccessResponse(res)
 		} else {
 			controller.JsonErrorResponse(err.Code, err.Message, res)
 		}
 	}
+}
+
+func callControllerMethod(controller ControllerInterface, method string, args ...interface{}) (interface{}, *ResponseException) {
+	res, err := utils.CallMethod2(controller, method, args...)
+
+	errKind := reflect.ValueOf(err).Kind()
+	if errKind == reflect.Ptr && !reflect.ValueOf(err).IsNil() {
+		switch reflect.TypeOf(err).Elem().Name() {
+		case "errorString":
+			return res, NewResponseException(-1, err.(error).Error())
+		case "ResponseException":
+			return res, err.(*ResponseException)
+		default:
+			return res, NewResponseException(-1, fmt.Sprintf("%#v", err))
+		}
+	} else if errKind == reflect.String && err != "" {
+		return res, NewResponseException(-1, err.(string))
+	}
+
+	return res, nil
 }
