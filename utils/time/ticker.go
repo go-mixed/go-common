999a8d54b85839cb5085a494e740c2f5ca4b9671
fly <fly@load-page.com>
2023-01-10 17:30:53 +0800

    add maxConcurrentRunningCount to Ticker

--- utils/time/ticker.go
+++ utils/time/ticker.go
@@ -7,14 +7,20 @@ type Ticker struct {
 
 	callback func()
 	stopCh   chan struct{}
+	// 同时运行的任务，如果超过正maxConcurrentRunningCount，则等待下个周期
+	runningCh chan struct{}
 }
 
-func NewTicker(d time.Duration, callback func()) *Ticker {
+func NewTicker(interval time.Duration, callback func(), maxConcurrentRunningCount int) *Ticker {
+	if maxConcurrentRunningCount <= 0 {
+		maxConcurrentRunningCount = 1
+	}
 	t := &Ticker{
-		Ticker:   time.NewTicker(d),
+		Ticker:   time.NewTicker(interval),
 		callback: callback,
 
-		stopCh: make(chan struct{}),
+		stopCh:    make(chan struct{}),
+		runningCh: make(chan struct{}, maxConcurrentRunningCount),
 	}
 
 	go t.run()
@@ -27,9 +33,21 @@ func (t *Ticker) run() {
 		case <-t.stopCh:
 			break
 		case <-t.Ticker.C:
-			t.callback()
+			select {
+			case t.runningCh <- struct{}{}: // 能插入任务
+				go t.handle() // 运行任务
+			default: // 丢弃任务
+
+			}
 		}
 	}
+}
+
+func (t *Ticker) handle() {
+	defer func() { // 运行完毕后腾出空位
+		<-t.runningCh
+	}()
+	t.callback()
 }
 
 func (t *Ticker) Stop() {
---
+++ utils/time/ticker_test.go
@@ -0,0 +1,35 @@
+package time_utils
+
+import (
+	"sync/atomic"
+	"testing"
+	"time"
+)
+
+func TestTicker(t *testing.T) {
+	var i atomic.Int32
+	var times []time.Time
+	ticker := NewTicker(100*time.Millisecond, func() {
+		times = append(times, time.Now())
+		t.Logf("%d - now: %s", i.Add(1), time.Now())
+		time.Sleep(1 * time.Second)
+
+	}, 3)
+
+	time.Sleep(5 * time.Second)
+	ticker.Stop()
+
+	if len(times) != 15 {
+		t.Errorf("times length must be 15")
+		t.Fail()
+	}
+
+	if delta := times[14].Sub(times[0]); delta >= 500*time.Millisecond {
+		t.Errorf("duration delta must be 5s")
+		t.Fail()
+	} else {
+		t.Logf("delta %.4f", delta.Seconds())
+	}
+
+	time.Sleep(1 * time.Second)
+}
